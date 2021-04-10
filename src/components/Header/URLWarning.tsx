c38b0a7ee8da6cab490bd1eb44e4e62c3bf523be
JayJay <81240900+hijayjay@users.noreply.github.com>
2021-04-10 23:40:00 +0700

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,15 +30,15 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>exchange.sushi.com</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>safechain.finance</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'exchange.sushi.com' ? (
+  ) : window.location.hostname === 'safechian.finance' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>exchange.sushi.com</code> - bookmark
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>safechian.finance</code> - bookmark
         it to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
